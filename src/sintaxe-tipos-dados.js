0f8eee6a9a90d668da7839e261d1fb5323386149
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-19 13:16:58 -0300

    feat: add concepts about syntax and data types

---
+++ src/sintaxe-tipos-dados.js
@@ -0,0 +1,14 @@
+// Comentário de linha única
+
+/** 
+ Comentário 
+ de múltiplas 
+ linhas 
+*/
+
+// Tipos de dados
+let numero = 10; // Number
+let texto = "Olá, Mundo!"; // String
+let booleano = true; // Boolean
+let indefinido = undefined; // Undefined
+let nulo = null; // Null
