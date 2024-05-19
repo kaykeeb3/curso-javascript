034de480b407d2e244a1e763fef854c007d135f4
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-19 13:34:01 -0300

    feat: add arithmetic, comparison and logical operators

---
+++ src/operadores.js
@@ -0,0 +1,16 @@
+// Operadores Aritméticos
+let soma = 5 + 5; // 10
+let subtracao = 5 - 3; // 2
+let multiplicacao = 5 * 3; // 15
+let divisao = 5 / 3; // 1.6667
+
+// Operadores de Comparação
+let igual = 5 === "5"; // true (igualdade solta)
+let estritamenteIgual = 5 === 5; // false (igualdade escrita)
+let diferente = 5 != "5"; // false
+let estritamenteDiferente = 5 !== 5; // true
+
+// Operadores Lógicos
+let e = true && false; // false
+let ou = true || false; // true
+let nao = !true; // false
