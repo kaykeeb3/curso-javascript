67777cc53ea4b5ab401212a885279198eaf6255f
kaykeeb3 <kaykeloioladev@gmail.com>
2024-05-21 13:36:43 -0300

    feat: add button animation, return to top of page

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Botão de Voltar ao Topo
+const backToTopButton = document.getElementById("backToTop");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "block";
+  } else {
+    backToTopButton.style.display = "none";
+  }
+});
+
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
